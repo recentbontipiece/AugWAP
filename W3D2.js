a00b92c574b61c00cc5ed9e9dbeb8c85dd8ce2e2
recentbontipiece <johnstyves@gmail.com>
2019-08-07 15:53:58 -0500

    Add files via upload

--- W3D2.js
+++ W3D2.js
@@ -1,20 +1,84 @@
 
 (function () {
-   let timer   = null;
-   let element = $("#cicle");
+    "use stric"
+   let timer      =null;
+   let element    =$("#cicle");
+   let container  =$("#circleContainer");
+   let backg      =["yellow","darkgray","darkslategrey","blueviolet","aqua"]
+   let widtd;
+   let growth;
+   let vrate;
+   let grointervale;
 
 
-   function resize(){
-       let size = parseInt(element.css("font-size"));
-       element.css("font-size",(size+10)+"px");
-       console.log("Info "+size);
+   $("body").on('click','.circlesnew',remove);
+
+    $("#btSize").on("click",function () {
+
+      let newContainer = $("<div>");
+
+       widtd          =$("#vwidth").val();
+       growth         =$("#vamount").val();
+       vrate          =$("#vrate").val();
+       grointervale   =$("#grointervale").val();
+
+       console.log("widtd "+widtd);
+       console.log("growth "+growth);
+       console.log("vrate "+vrate);
+       console.log("grointervale "+grointervale);
+
+       while(grointervale > 0){
+           newContainer.append($("<div>",{
+               "class":"circlesnew",
+               "css": {
+               "width": widtd+"px",
+               "height":widtd+"px",
+               "border-radius": "50%",
+               "background": backg[getRandomArbitrary(0,backg.length)],
+               "position": "absolute",
+               "bottom"  :getRandomArbitrary(0,30),
+               "left"    :getRandomArbitrary(30,70)+"%",
+             }
+           }));
+
+           grointervale--;
+
+       }
+       container.append(newContainer);
+       element.remove();
+
+
+
+        console.log("Answer "+vrate);
+        if(timer !=null){
+            clearInterval(timer);
+            timer = null;
+        }
+        timer = setInterval(resize,vrate)
+
+   });
+
+    function remove() {
+        console.log("Remove");
+       $(this).remove();
+    }
+
+   function getRandomArbitrary(min, max) {
+     return Math.floor(Math.random() * (max - min) + min);
    }
-   timer = setInterval(resize, 250);
 
-    element.onclick(function () {
+    function resize(){
+     $(".circlesnew").css("width",function (idx, old) {
+         console.log(parseInt(old) +parseInt(growth));
+        return parseInt(old) +parseInt(growth)+"px";
+     }).css("height",function (idx, old) {
+         console.log(parseInt(old) +parseInt(growth));
 
-        $(this).remove();
-    });
+         return parseInt(old) +parseInt(growth)+"px";
+     });
+   }
+
+
 
 
 
